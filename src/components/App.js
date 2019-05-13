2fe3981cef7e41f90c343408c7c19392eb85b927
Julien <j_dommel@yahoo.fr>
2019-05-13 14:36:56 +0200

    after cleanup, added home

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./Home";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Route exact path="/" component={Home} />
+      </Router>
+    );
+  }
+}
+
+export default App;

849f85f14f2eb6eb995f0ac17d553cad67c51e20
Eric ACKERMANN <49131028+Eric-ACKERMANN@users.noreply.github.com>
2019-05-14 12:03:20 +0200

    update header

---
+++ src/components/HeaderAnnounce.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HeaderAnnounce(props) {
+  return (
+    <div className="header-annonce">
+      <i class="far fa-plus-square fa-2x" />
+      <span className="header-annonce-text">Déposer une annonce</span>
+    </div>
+  );
+}
+export default HeaderAnnounce;

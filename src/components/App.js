2184549aee3cc2e2df0607dcf5ac8ef0ac471032
Julien <j_dommel@yahoo.fr>
2019-05-14 16:55:26 +0200

    updated form and search

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,30 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Home";
 import Offer from "./Offer";
 import Header from "./Header";
+import SignUp from "./SignUp";
+import Cookies from "js-cookie";
 
 class App extends React.Component {
+  state = {
+    userData: null
+  };
+
   render() {
+    if (this.state.userData !== null) {
+      Cookies.set("token", this.state.userData.token);
+    }
+
     return (
       <Router>
         <Header />
         <Route exact path="/" component={Home} />
         <Route path="/offer/:id" component={Offer} />
+        <Route
+          path="/user/sign-up"
+          render={props => (
+            <SignUp onSignUp={user => this.setState({ userData: user })} />
+          )}
+        />
       </Router>
     );
   }

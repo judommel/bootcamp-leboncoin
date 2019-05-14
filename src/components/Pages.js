afa7d1d6a9a58ea7a5c5c8ed81cf929b87505d4e
Julien <j_dommel@yahoo.fr>
2019-05-14 11:59:06 +0200

    Pages updated

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Pages(props) {
-  const { count, onPageClick } = props;
+  const { count, onPageClick, onNextPage, onPreviousPage, current } = props;
 
   const pages = Math.ceil(count / 25);
 
@@ -12,15 +12,23 @@ function Pages(props) {
   }
 
   return (
-    <ul className="page-links">
-      {pageLinks.map(element => {
-        return (
-          <li key={element} onClick={toto => onPageClick(element)}>
-            {element}
-          </li>
-        );
-      })}
-    </ul>
+    <div className="container page-flex">
+      <i className="fas fa-chevron-left" onClick={() => onPreviousPage()} />
+      <ul className="page-links">
+        {pageLinks.map(element => {
+          return (
+            <li
+              key={element}
+              className={current === element ? "current-page" : "page-li"}
+              onClick={toto => onPageClick(element)}
+            >
+              {element}
+            </li>
+          );
+        })}
+      </ul>{" "}
+      <i className="fas fa-chevron-right" onClick={() => onNextPage()} />
+    </div>
   );
 }
 
